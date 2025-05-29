6e14a6bdb979c22da2d03ac0e95169acf0f5f9de
Rahul <rlbishwokarma@gmail.com>
2025-05-29 13:29:30 +0545

    Design homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Fitness Gym",
   description: "A gym and fitness website",
 };
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -15,7 +18,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={` antialiased`}>{children}</body>
+        <body className={`${montserrat.className} antialiased`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { ArrowDown } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="p-5 flex flex-col gap-5 ">
-      <h1 className="text-red-500">Home Page</h1>
-      <Button>Cick me</Button>
+    <div className="home-parent text-white  py-10 px-20">
+      <div className="flex justify-between items-center">
+        <h1 className=" text-2xl font-bold  uppercase">Fitness.Gym</h1>
+        <Button variant={"outline"} className=" cursor-pointer">
+          Sign-in
+        </Button>
+      </div>
+      <div className="flex flex-col gap-5 justify-center items-center h-[80vh]">
+        <h1 className="text-6xl">
+          <strong>Fitness.</strong>
+          <strong className="text-primary">Gym</strong>
+        </h1>
+        <p className="text-sm font-semibold text-gray-300">
+          A perfect gym for you to get fit and healthy with the best trainers
+          and equipments.
+        </p>
+        <Button>Explores Plan</Button>
+        <ArrowDown
+          size={20}
+          color="gray"
+          className="animate-bounce cursor-pointer mt-5"
+        />
+      </div>
     </div>
   );
 }
