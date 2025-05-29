6bca40a09504496e669f9adb834de3d69f39c20f
Rahul <rlbishwokarma@gmail.com>
2025-05-29 16:40:06 +0545

    Setup supabase

---
+++ src/config/supabase-config.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_API_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
