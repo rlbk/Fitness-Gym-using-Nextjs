2acdf7b5b3235c4d8e7aa5f8e6b9e5481507204f
Rahul <rlbishwokarma@gmail.com>
2025-05-29 15:20:38 +0545

    Add signin and signout redirectUrl and design simple account page

---
+++ src/app/(private)/account/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+const Page = async () => {
+  const result = await currentUser();
+  const name = `${result?.firstName} ${result?.lastName}`;
+  const clerkUserId = result?.id;
+  const email = result?.emailAddresses[0]?.emailAddress;
+  return (
+    <div className="p-5">
+      <h1>Account Page</h1>
+      <UserButton fallback="/" afterSwitchSessionUrl="/" />
+      <h1>Clerk User Name: {name}</h1>
+      <h1>Clerk User email: {email}</h1>
+      <h1>Clerk User Email: {email}</h1>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/home-navbar.tsx
+++ src/components/home-navbar.tsx
@@ -38,9 +38,17 @@ const HomeNavebar = () => {
           </SheetHeader>
 
           {form === "sign-up" ? (
-            <SignUp routing="hash" signInUrl="/?form=sign-in" />
+            <SignUp
+              routing="hash"
+              signInUrl="/?form=sign-in"
+              fallbackRedirectUrl={"/account"}
+            />
           ) : (
-            <SignIn routing="hash" signUpUrl="/?form=sign-up" />
+            <SignIn
+              routing="hash"
+              signUpUrl="/?form=sign-up"
+              fallbackRedirectUrl={"/account"}
+            />
           )}
         </SheetContent>
       </Sheet>
