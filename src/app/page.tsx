566608eb1f87a79befb792b9e79ff38ccca30917
Rahul <rlbishwokarma@gmail.com>
2025-05-29 13:00:46 +0545

    Setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className="p-5 flex flex-col gap-5 ">
+      <h1 className="text-red-500">Home Page</h1>
+      <Button>Cick me</Button>
+    </div>
   );
 }
